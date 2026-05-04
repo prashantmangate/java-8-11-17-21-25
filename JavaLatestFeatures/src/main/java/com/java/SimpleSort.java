78d1fd22d3caeb29f38240c559b5a4e59c795312
Prashant Mangate <prashant_mangate@epam.com>
2026-05-03 18:46:23 -0700

    updated to remove warnings

--- JavaLatestFeatures/src/main/java/com/java/SimpleSort.java
+++ JavaLatestFeatures/src/main/java/com/java/SimpleSort.java
@@ -1,12 +1,14 @@
 package com.java;
 
+import java.util.Arrays;
+
 /* Simple bubble sort program with n square time complexity
-*
+* Bubble sort
 */
 public class SimpleSort {
     public static void main(String [] args) {
         int [] arr = new int[]{10,4,2,1,6,3,5,13,7,9};
-        int temp = 0;
+        int temp;
         for(int i =0 ; i<arr.length;i++) {
             for(int j =i+1 ; j<arr.length;j++){
                 if(arr[i]>arr[j]){
@@ -16,9 +18,6 @@ public class SimpleSort {
                 }
             }
         }
-
-        for (int i=0;i<arr.length;i++){
-            System.out.println(arr[i]);
-        }
+        Arrays.stream(arr).forEach(System.out::println);
     }
 }
