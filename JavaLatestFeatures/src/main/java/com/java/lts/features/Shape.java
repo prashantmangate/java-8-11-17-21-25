acf4728121370da5336d5c6dadbbe901175c2616
Prashant Mangate <prashant_mangate@epam.com>
2026-05-09 16:50:58 -0700

    Add class - sealed keyword

---
+++ JavaLatestFeatures/src/main/java/com/java/lts/features/Shape.java
@@ -0,0 +1,24 @@
+package com.java.lts.features;
+
+/*
+/ Sealed Class
+ */
+public sealed class Shape permits Circle, Square{
+
+    public double getArea(){
+        return 0.0;
+    }
+
+}
+final class Circle extends Shape{
+    double pi = 3.14;
+    public double getArea(double r){
+        return pi*r*r;
+    }
+}
+final class Square extends Shape {
+
+    public double getArea(double e){
+        return e*e;
+    }
+}
